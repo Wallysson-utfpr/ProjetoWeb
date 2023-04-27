3be3326d4bf7e713c5594e42bea03c25494b9e1c
Wallysson Barbosa <wallysson.mateus.barbosa@hotmail.com>
2023-04-27 15:01:41 -0300

    add server

--- src/server.js
+++ src/server.js
@@ -1,2 +1,20 @@
 const express = require("express");
 const app = express();
+const conectDatabase = require("./database/db");
+const usuarioRouter = require("./routes/rotasUser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors()); //
+app.use(usuarioRouter);
+
+app.use(express.json());
+
+conectDatabase();
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
