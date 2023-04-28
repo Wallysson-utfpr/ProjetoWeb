5fc8abbd57ca6412ebdc555218bb54a9378bb81a
joseroberto <joseroberto@alunos.utfpr.edu.br>
2023-04-28 20:36:40 -0300

    JRMS

--- src/components/ListaMoeda.js
+++ src/components/ListaMoeda.js
@@ -44,15 +44,37 @@ function ListaMoeda() {
         Voltar
       </Link>
       <ul>
+
+      <table border="1">
+            <tr>
+              <th width="200"> Moeda:</th>
+              <th width="200"> Valor Máximo</th>
+              <th width="200"> Valor Mínimo</th>
+            </tr>
+            </table>
+
         {moedas.map((moeda) =>
           busca !== "" &&
           moeda.nome.toLowerCase().indexOf(busca.toLowerCase()) ===
             -1 ? null : (
-            <li key={moeda._id}>
-              <span className="corWhite">Nome da moeda:</span> {moeda.nome}{" "}
-              <br /> <span className="corWhite">Máxima:</span> {moeda.alta}{" "}
-              <br /> <span className="corWhite">Mínima:</span> {moeda.baixa}
-            </li>
+           // <li key={moeda._id}>
+           //   <span className="corWhite">Nome da moeda:</span> {moeda.nome}{" "}
+           //   <br /> <span className="corWhite">Máxima:</span> {moeda.alta}{" "}
+           //   <br /> <span className="corWhite">Mínima:</span> {moeda.baixa}
+           // </li>
+          
+           <table border="1">
+            
+            <tr>
+              <td >{moeda.nome}</td>
+              <td>{moeda.alta} </td>
+              <td>{moeda.baixa}</td>
+            </tr>
+    
+            </table>
+          
+          
+          
           )
         )}
       </ul>
