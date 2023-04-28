e6a232f94acfd13e38f7120e2381976cebed8181
José Roberto <101138497+joseroberto-utfpr@users.noreply.github.com>
2023-04-28 18:22:11 -0300

    Update Cadastro.js

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -1,3 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "../axiosConfig";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../styles/cadastro.css";
+
+function Cadastro() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const handleRedirect = () => {
+    window.location.href = "/";
+  };
