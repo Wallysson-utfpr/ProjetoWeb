e956cb93f91c0c79f8da936703c9dae18c6e30a0
Wallysson Barbosa <wallysson.mateus.barbosa@hotmail.com>
2023-05-01 19:35:46 -0300

    att

--- src/components/ListaMoeda.js
+++ src/components/ListaMoeda.js
@@ -44,39 +44,28 @@ function ListaMoeda() {
         Voltar
       </Link>
       <ul>
+        <table border="2">
+          <tr>
+            <th width="200"> Moeda:</th>
+            <th width="200"> Valor Máximo</th>
+            <th width="200"> Valor Mínimo</th>
+          </tr>
+        </table>
 
-      <table border="2">
-            <tr>
-              <th width="200"> Moeda:</th>
-              <th width="200"> Valor Máximo</th>
-              <th width="200"> Valor Mínimo</th>
-            </tr>
-            </table>
-
-          {moedas.map((moeda) =>
+        {moedas.map((moeda) =>
           busca !== "" &&
           moeda.nome.toLowerCase().indexOf(busca.toLowerCase()) ===
             -1 ? null : (
-           // <li key={moeda._id}>
-           //   <span className="corWhite">Nome da moeda:</span> {moeda.nome}{" "}
-           //   <br /> <span className="corWhite">Máxima:</span> {moeda.alta}{" "}
-           //   <br /> <span className="corWhite">Mínima:</span> {moeda.baixa}
-           // </li>
-          
-           <table border="1">
-            
-            <tr>
-              <td width="200">{moeda.nome}</td>
-              <td width="200">{moeda.alta} </td>
-              <td width="200">{moeda.baixa}</td>
-            </tr>
-    
+            <table border="1">
+              <tr>
+                <td width="200">{moeda.nome}</td>
+                <td width="200">{moeda.alta} </td>
+                <td width="200">{moeda.baixa}</td>
+              </tr>
             </table>
-                    
           )
         )}
       </ul>
-      
     </div>
   );
 }
